a4be73dec914432e4ba00b898944f95034205d78
Chuuch <daniel.chuchulev96@gmail.com>
2023-12-18 12:22:11 +0200

    Hero back to top button

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,9 @@ import Experience from '@/components/Experience';
 import Projects from '@/components/Projects';
 import Skills from '@/components/Skills';
 import ContactMe from '@/components/ContactMe';
+import Link from 'next/link';
+import Profile from '../public/Profile.jpg'
+import Image from 'next/image';
 
 const Home: NextPage = () => {
  return (
@@ -43,6 +46,14 @@ const Home: NextPage = () => {
       <section id='contact' className='snap-start'>
         <ContactMe />
       </section>
+
+      <Link href='#hero'>
+        <footer className='sticky bottom-5 w-full cursor-pointer'>
+          <div className='flex items-center justify-center'>
+            <Image src={Profile} alt='profile' className='w-10 h-10 filter grayscale hover:grayscale-0 cursor-pointer rounded-full justify-center'/>
+          </div>
+        </footer>
+      </Link>
       </div>
     )
 }
