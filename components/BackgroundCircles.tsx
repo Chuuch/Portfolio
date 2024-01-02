3525a58cedef75046db479493d2a8cae592c44ca
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-02 03:28:14 -0800

    removed background circles

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -13,9 +13,6 @@ function BackgroundCircles({}: Props) {
         className='relative flex justify-center items-center'>
 
         <div className='absolute border border-[#008080] rounded-full h-[200px] w-[200px] mt-52 animate-ping'/>
-        <div className='rounded-full border border-[#333333] h-[300px] w-[300px] absolute mt-52'/>
-        <div className='rounded-full border border-[#333333] h-[500px] w-[500px] absolute mt-52'/>
-        <div className='rounded-full border border-[#008080] h-[800px] w-[800px] absolute mt-52'/>
         <div />
     </motion.div>
   )
