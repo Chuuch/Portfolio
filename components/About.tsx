1a90900ef791a59a40bcda4155550b492a17d10e
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-02 03:27:56 -0800

    reworked about section

--- components/About.tsx
+++ components/About.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
-import Batak from '../public/Batak.jpg'
+import about2 from '../public/about2.jpg'
 import { motion } from 'framer-motion'
 
 type Props = {}
@@ -20,9 +20,9 @@ export default function About({}: Props) {
             whileInView={{ opacity: 1, x: 0}}
             viewport={{ once: true }}>
 
-            <Image src={Batak} alt='batak'
+            <Image src={about2} alt='about-photo'
             className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg
-                        md:w-[1600px] md:h-64 xl:w-[2500px] xl:h-[600px] '/>
+                        md:w-[1600px] md:h-64 xl:w-[400px] xl:h-[500px]'/>
        </motion.div>
 
        <motion.div 
@@ -30,16 +30,21 @@ export default function About({}: Props) {
           whileInView={{ opacity: 1 }}
           transition={{ duration: 1.5 }}
           className='space-y-10 px-0 md:px-10'>
-            <h4 className='text-4xl font-semibold text-white'>
-                Here is a <span className='underline decoration-[#008080]/50'>little</span> background
+            <h4 className='text-4xl font-semibold text-teal-600'>
+                Who am <span className='underline decoration-[#008080]/50'>I</span>?
             </h4>
-            <p className='text-sm xl:text-base md:text-base text-white'>
-                Hello there! My name is Daniel and this is my portfolio.
-                I got into coding the last few years as a hobby in my free time building
-                basic projects and learning new technologies.
-                Programming sparked my interest as there is a certain beauty
-                in building something with a purpose. Personally, I see it as a never ending adventure because you can
-                always keep learning and creating new things!
+            <p className='text-sm xl:text-base md:text-base  whitespace-pre-line xl:w-[600px]'>
+            Greetings, I am Daniel, and welcome to my portfolio. 
+            Over the past year, I have immersed myself in the realm of coding, 
+            transforming it from a casual hobby into a dedicated pursuit of a career as a software developer.
+            Throughout this journey, I have undertaken the development of fundamental projects
+             and acquired proficiency in emerging technologies.
+            <br/>
+            <br />
+            The allure of programming lies in its inherent elegance and the ability to construct purposeful solutions. 
+            For me, it represents an ongoing odyssey, where the potential for learning and innovation remains boundless. 
+            I am passionate about the dynamic nature of this field, 
+            continually seeking opportunities to expand my knowledge and craft innovative solutions.
             </p>
        </motion.div>
     </div>
