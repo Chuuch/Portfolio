a5596e18f3fa04bb984f38fc4780b3c8ef20a4cf
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-03 02:00:32 -0800

    Mapped experience card info from backend

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
 import { motion } from 'framer-motion'
 import Image from 'next/image'
@@ -9,49 +11,52 @@ import javascript from '../public/javascript.png'
 import html from '../public/html.png'
 import css from '../public/css.png'
 import certificate from '../public/certificate.png'
+import { Experience } from '@/typings'
+import { urlFor } from '@/sanity'
 
-type Props = {}
+type Props = {
+    experience: Experience;
+}
 
-function ExperienceCard({}: Props) {
+function ExperienceCard({ experience }: Props) {
   return (
     <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[600px]
                         snap-center bg-gray-100 p-10 cursor-pointer transition-opacity duration-200 overflow-hidden'>
-        <motion.div
+        <motion.img
             initial={{ y: -100, opacity: 0 }}
             transition={{ duration: 1.2 }}
             whileInView={{ opacity: 1, y: 0}}
-            viewport={{ once: true }}>
-            <Image src={telerik} alt='telerik'
-                    className='h-32 w-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center'/>
-        </motion.div>
+            viewport={{ once: true }}
+            src={urlFor(experience?.companyImage).url()} alt='telerik'
+            className='h-32 w-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center'>
+            
+                    
+        </motion.img>
 
         <div className='px-0 md:px-10 flex flex-col items-start'>
             <h4 className='text-green-500 text-4xl font-light'>
-                Alpha JavaScript Track
+                {experience?.jobTitle}
             </h4>
 
             <p className='font-bold text-2xl mt-1'>
-                Telerik Academy
+                {experience?.company}
             </p>
 
             <div className='flex space-x-2 my-2'>
-                <Image src={react} alt='react'
-                        className='h-8 w-9'/>
-                <Image src={tailwind} alt='tailwind'
-                        className='h-8 w-10 rounded-full'/>
-                <Image src={firebase} alt='firebase'
-                        className='h-8 w-8'/>
-                <Image src={javascript} alt='javascript'
-                        className='h-8 w-8'/>
-                <Image src={html} alt='html'
-                        className='h-8 w-8'/> 
-                <Image src={css} alt='css'
-                        className='h-8 w-8'/>
+                {experience.technologies.map((technology) => (
+                    <img 
+                        key={technology._id}
+                        className='h-8 w-8 rounded-full'
+                        src={urlFor(technology.image).url()}/>
+                ))}
 
             </div>
 
             <p className='uppercase py-5 text-gray-500'>
-                06 JUL 2023 - 19 DEC 2023
+                {new Date(experience.dateStarted).toDateString()} - {" "}
+                {experience.isCurrentlyWorkingHere
+                ? 'Present'
+                : new Date(experience.dateEnded).toDateString()}
             </p>
 
             <ul className='list-disc space-y-4 ml-5 text-lg'>
