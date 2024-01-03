131b5ef56c2ba770e97d2e267835d698ee90bc26
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-03 13:20:12 -0800

    Theme switch component

---
+++ components/ThemeSwitch.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { BsMoon, BsSun } from 'react-icons/bs';
+import { useEffect, useState } from 'react';
+
+type Theme = 'light' | 'dark';
+
+const ThemeSwitch = () => {
+	const [theme, setTheme] = useState<Theme>('light');
+
+	const toggleTheme = () => {
+		if (theme === 'light') {
+			setTheme('dark');
+			window.localStorage.setItem('theme', 'dark');
+		} else {
+			setTheme('light');
+			window.localStorage.setItem('theme', 'light');
+		}
+	};
+
+	useEffect(() => {
+		const localTheme = window.localStorage.getItem('theme') as Theme | null;
+
+		if (localTheme) {
+			setTheme(localTheme);
+		} else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+			setTheme('dark');
+		}
+	}, []);
+
+	return (
+		<button
+			onClick={toggleTheme}
+			className="flex items-center cursor-pointer hover:scale-110 text-teal-700"
+		>
+			{theme === 'light' ? <BsMoon size={20} /> : <BsSun size={20} />}
+		</button>
+	);
+};
+
+export default ThemeSwitch;
