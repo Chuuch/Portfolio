525964d57a96b243a730560865163713c0d986b3
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-03 03:38:49 -0800

    Mapped projects and added styling

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,14 +1,16 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import Image from 'next/image';
 import React from 'react';
-import netflix_clone_app from '../public/netflix_clone_app.png';
-import Forum from '../public/Forum.png'
 import { motion } from 'framer-motion';
+import { Project } from '@/typings';
+import { urlFor } from '@/sanity';
 
-type Props = {};
+type Props = {
+	projects: Project[];
+};
 
-function Projects({}: Props) {
-	const projects = [1, 2, 3, 4, 5];
-
+function Projects({ projects }: Props) {
 	return (
 		<motion.div
 			initial={{ opacity: 0 }}
@@ -25,31 +27,32 @@ function Projects({}: Props) {
 				className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20
                         scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#008080]/80"
 			>
-				{projects.map((project, i) => (
+				{projects?.map((project, i) => (
 					<div key={i} className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
-						<motion.div initial={{ y: -300, opacity: 0 }} transition={{ duration: 1.2 }} whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true }}
-						>
-							<Image
-								src={Forum}
+						<motion.img initial={{ y: -300, opacity: 0 }} transition={{ duration: 1.2 }} whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true }}
+								src={urlFor(project?.image).url()}
 								alt="Forum"
-								className="xl:w-[700px] xl:h-[300px] rounded-md"
-							/>
-						</motion.div>
+								className="xl:w-[700px] xl:h-[300px] rounded-md">
+						</motion.img>
 
-						<div className="space-y-10 px-0 md:px-10 max-w-6xl">
-							<h4 className="text-4xl font-semibold text-center">
+						<div className="space-y-5 px-0 md:px-10 max-w-6xl">
+							<h4 className="lg:text-4xl text-xl md:text-2xl font-semibold text-center">
 								<span className="underline decoration-[#008080]/50">
 									Case Study {i + 1} of {projects.length}:
 								</span>{' '}
-								DevTalk Forum
+								{project?.title}
 							</h4>
-
+								<div className="flex items-center space-x-4 justify-center">
+									{project?.technologies.map((technology) => (
+										<img
+											className='lg:h-10 lg:w-10 h-8 w-8' 
+											key={technology._id}
+											src={urlFor(technology.image).url()}
+											alt=''/>
+									))}
+								</div>
 							<p className="text-sm md:text-base md:text-left text-center">
-							DevTalk Forum is a modern and efficient forum application for software development.
-							This project is built using React, Tailwind CSS, and Firebase Realtime Database to provide users with a seamless and responsive forum experience.
-							The forum includes features such as user registration, login, post creation, 
-							liking and commenting, profile picture uploads, post related notifications and an advanced search functionality.
-							Additionally, users can use the contact form to reach out to the project team and apply for admin privileges or enquire about any issues they might face.
+							{project?.summary}
 							</p>
 						</div>
 					</div>
