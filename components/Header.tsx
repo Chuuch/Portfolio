544952b3d1566cf9fc835a68b1c9ec2d58297d9b
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-03 02:00:47 -0800

    Mapped header info from backend

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,33 +1,51 @@
-import React from 'react'
-import { SocialIcon } from 'react-social-icons'
-import { motion } from 'framer-motion'
+import React from 'react';
+import { SocialIcon } from 'react-social-icons';
+import { motion } from 'framer-motion';
+import { Social } from '@/typings';
 
-type Props = {}
+type Props = {
+	socials: Social[];
+};
 
-export default function Header({}: Props) {
-  return (
-    <header className='sticky top-0 flex items-start justify-between max-w-7xl mx-auto z-20
-                        xl:items-center p-5'>
-      <motion.div
-        initial={{ x: -500, opacity: 0, scale: 0.5 }}
-        animate={{ x: 0, opacity: 1, scale: 1 }}
-        transition={{ duration: 1.5 }}
-        className='flex flex-row items-center'>
-        <SocialIcon url='https://www.facebook.com/daniel.chuchulev' fgColor='#4267B2' bgColor='transparent' className='hover:scale-150'/>
-        <SocialIcon url='https://www.instagram.com/chuchulev/' fgColor='#C13584' bgColor='transparent'className='hover:scale-150'/>
-        <SocialIcon url='https://www.linkedin.com/in/chuchulev/'fgColor='#0077B5' bgColor='transparent'className='hover:scale-150'/>
-        <SocialIcon url='https://twitter.com/chuchulev'fgColor='#1DA1F2' bgColor='transparent'className='hover:scale-150'/>
-        <SocialIcon url='https://github.com/Chuuch' fgColor='teal' bgColor='transparent'className='hover:scale-150'/>
-      </motion.div>
+export default function Header({ socials }: Props) {
+	return (
+		<header
+			className="sticky top-0 flex items-start justify-between max-w-7xl mx-auto z-20
+                        xl:items-center p-5"
+		>
+			<motion.div
+				initial={{ x: -500, opacity: 0, scale: 0.5 }}
+				animate={{ x: 0, opacity: 1, scale: 1 }}
+				transition={{ duration: 1.5 }}
+				className="flex flex-row items-center"
+			>
+				{socials.map((social) => (
+					<SocialIcon
+            key={social._id}
+						url={social.url}
+						fgColor="#008080"
+						bgColor="transparent"
+						className="hover:scale-150"
+					/>
+				))}
+			</motion.div>
 
-      <motion.div
-        initial={{ x: 500, opacity: 0, scale: 0.5 }}
-        animate={{ x: 0, opacity: 1, scale: 1 }}
-        transition={{ duration: 1.5 }}
-        className='flex flex-row items-center cursor-pointer'>
-        <SocialIcon className='cursor-pointer hover:scale-150' network='email' fgColor='#008080' bgColor='transparent'/>
-        <p className='uppercase hidden md:inline-flex text-sm text-teal-600'>Get In Touch</p>
-      </motion.div>
-    </header>
-  )
+			<motion.div
+				initial={{ x: 500, opacity: 0, scale: 0.5 }}
+				animate={{ x: 0, opacity: 1, scale: 1 }}
+				transition={{ duration: 1.5 }}
+				className="flex flex-row items-center cursor-pointer"
+			>
+				<SocialIcon
+					className="cursor-pointer hover:scale-150"
+					network="email"
+					fgColor="#008080"
+					bgColor="transparent"
+				/>
+				<p className="uppercase hidden md:inline-flex text-sm text-teal-600">
+					Get In Touch
+				</p>
+			</motion.div>
+		</header>
+	);
 }
