8acfb9913194c3384fa6116482ea2ca0f183d05e
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-03 03:38:31 -0800

    Applied styling changes to Experience card

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -20,7 +20,7 @@ type Props = {
 
 function ExperienceCard({ experience }: Props) {
   return (
-    <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[600px]
+    <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[450px] md:w-[500px] xl:w-[600px]
                         snap-center bg-gray-100 p-10 cursor-pointer transition-opacity duration-200 overflow-hidden'>
         <motion.img
             initial={{ y: -100, opacity: 0 }}
@@ -34,11 +34,11 @@ function ExperienceCard({ experience }: Props) {
         </motion.img>
 
         <div className='px-0 md:px-10 flex flex-col items-start'>
-            <h4 className='text-green-500 text-4xl font-light'>
+            <h4 className='text-green-500 lg:text-4xl md:text-3xl text-2xl font-light'>
                 {experience?.jobTitle}
             </h4>
 
-            <p className='font-bold text-2xl mt-1'>
+            <p className='font-bold text-lg md:text-2 lg:text-2xl mt-1'>
                 {experience?.company}
             </p>
 
@@ -52,18 +52,19 @@ function ExperienceCard({ experience }: Props) {
 
             </div>
 
-            <p className='uppercase py-5 text-gray-500'>
+            <p className='uppercase py-5 text-gray-500 text-sm md:text-base'>
                 {new Date(experience.dateStarted).toDateString()} - {" "}
                 {experience.isCurrentlyWorkingHere
                 ? 'Present'
                 : new Date(experience.dateEnded).toDateString()}
             </p>
 
-            <ul className='list-disc space-y-4 ml-5 text-lg'>
-                <li>Core Programming Fundamentals</li>
-                <li>Object Oriented Programming</li>
-                <li>Data Structures & Algorithms</li>
-                <li>Web Development</li>
+            <ul className='list-disc space-y-4 ml-5 text-base md:text-lg'>
+               {experience.points.map((point, index) => (
+                <li key={index}>
+                    {point}
+                </li>
+               ))}
             </ul>
 
         </div>
