65bf4f0239595f3815e072510663d9cff46040e0
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-03 02:01:44 -0800

    Defined & assigned props to components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,32 @@
 /* eslint-disable react/no-unescaped-entities */
-import type { NextPage } from 'next';
+import type { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 import About from '@/components/About';
-import Experience from '@/components/Experience';
+import WorkExperience from '@/components/WorkExperience';
 import Projects from '@/components/Projects';
 import Skills from '@/components/Skills';
 import ContactMe from '@/components/ContactMe';
 import Link from 'next/link';
 import Profile from '../public/Profile.jpg'
 import Image from 'next/image';
+import { Experience, PageInfo, Project, Skill, Social } from '@/typings';
+import { fetchPageInfo } from '@/utils/fetchPageInfo';
+import { fetchExperiences } from '@/utils/fetchExperiences';
+import { fetchSkills } from '@/utils/fetchSkills';
+import { fetchProjects } from '@/utils/fetchProjects';
+import { fetchSocials } from '@/utils/fetchSocials';
 
-const Home: NextPage = () => {
+type Props = {
+  pageInfo: PageInfo;
+  experiences: Experience[];
+  skills: Skill[];
+  projects: Project[];
+  socials: Social[];
+}
+
+const Home = ({pageInfo, experiences, skills, projects, socials}: Props) => {
  return (
       <div className='bg-[#fff] text-gray-500 h-screen snap-y snap-mandatory overflow-y-scroll z-0
                       overflow-x-hidden scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#008080]/80'>
@@ -20,18 +34,18 @@ const Home: NextPage = () => {
           <title>Daniel's Portfolio</title>
         </Head>
 
-      <Header />
+      <Header socials={socials}/>
 
        <section id='hero' className='snap-start'>
-        <Hero />
+        <Hero pageInfo={pageInfo}/>
        </section>
 
        <section id='about' className='snap-center'>
-        <About />
+        <About pageInfo={pageInfo}/>
        </section>
 
       <section id='experience' className='snap-center'>
-        <Experience />
+        <WorkExperience experiences={experiences}/>
       </section>
 
 
@@ -59,3 +73,22 @@ const Home: NextPage = () => {
 }
 
 export default Home;
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const pageInfo: PageInfo = await fetchPageInfo();
+  const experiences: Experience[] = await fetchExperiences();
+  const skills: Skill[] = await fetchSkills();
+  const projects: Project[] = await fetchProjects();
+  const socials: Social[] = await fetchSocials();
+
+  return {
+    props: {
+      pageInfo,
+      experiences,
+      skills,
+      projects,
+      socials,
+    },
+    revalidate: 10,
+  }
+}
