2cbcc76455b796125f0e2f67ba419ab20f9dcd08
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-26 13:28:03 -0800

    error handling in getStaticProps function to see where the '/' error comes from

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ import Skills from '@/components/Skills';
 import Qualifications from '@/components/Qualifications';
 import ContactMe from '@/components/ContactMe';
 import Link from 'next/link';
-import Profile from '@/public/Profile.jpg'
+import Profile from '@/public/Profile.jpg';
 import Image from 'next/image';
 import { Experience, PageInfo, Project, Skill, Social } from '@/typings';
 import { fetchPageInfo } from '@/utils/fetchPageInfo';
@@ -20,80 +20,90 @@ import { fetchProjects } from '@/utils/fetchProjects';
 import { fetchSocials } from '@/utils/fetchSocials';
 
 type Props = {
-  pageInfo: PageInfo;
-  experiences: Experience[];
-  skills: Skill[];
-  projects: Project[];
-  socials: Social[];
-}
+	pageInfo: PageInfo;
+	experiences: Experience[];
+	skills: Skill[];
+	projects: Project[];
+	socials: Social[];
+};
 
-const Home = ({pageInfo, experiences, skills, projects, socials}: Props) => {
- return (
-      <div className='bg-gray-200 dark:bg-[#00001a] text-gray-500 h-screen snap-y snap-mandatory overflow-y-scroll z-0
-                      overflow-x-hidden scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-blue-600 dark:scrollbar-thumb-blue-600'>
-        <Head>
-          <title>{pageInfo.name}</title>
-        </Head>
+const Home = ({ pageInfo, experiences, skills, projects, socials }: Props) => {
+	return (
+		<div
+			className="bg-gray-200 dark:bg-[#00001a] text-gray-500 h-screen snap-y snap-mandatory overflow-y-scroll z-0
+                      overflow-x-hidden scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-blue-600 dark:scrollbar-thumb-blue-600"
+		>
+			<Head>
+				<title>{pageInfo.name}</title>
+			</Head>
 
-      <Header socials={socials}/>
+			<Header socials={socials} />
 
-       <section id='hero' className='snap-start'>
-        <Hero pageInfo={pageInfo}/>
-       </section>
+			<section id="hero" className="snap-start">
+				<Hero pageInfo={pageInfo} />
+			</section>
 
-       <section id='about' className='snap-center'>
-        <About pageInfo={pageInfo}/>
-       </section>
+			<section id="about" className="snap-center">
+				<About pageInfo={pageInfo} />
+			</section>
 
-      <section id='experience' className='snap-center'>
-        <WorkExperience experiences={experiences}/>
-      </section>
+			<section id="experience" className="snap-center">
+				<WorkExperience experiences={experiences} />
+			</section>
 
+			<section id="skills" className="snap-start">
+				<Skills skills={skills} />
+			</section>
 
-      <section id='skills' className='snap-start'>
-        <Skills skills={skills}/>
-      </section>
+			<section id="projects" className="snap-start">
+				<Projects projects={projects} />
+			</section>
 
-      <section id='projects' className='snap-start'>
-        <Projects projects={projects}/>
-      </section>
+			<section id="qualifications" className="snap-start">
+				<Qualifications />
+			</section>
 
-      <section id='qualifications' className='snap-start'>
-        <Qualifications />
-      </section>
-    
-      <section id='contact' className='snap-start'>
-        <ContactMe />
-      </section>
+			<section id="contact" className="snap-start">
+				<ContactMe />
+			</section>
 
-      <Link href='#hero'>
-        <footer className='sticky bottom-5 w-full cursor-pointer'>
-          <div className='flex items-center justify-center'>
-            <Image src={Profile} alt='profile' className='w-10 h-10 filter grayscale hover:grayscale-0 cursor-pointer rounded-full justify-center'/>
-          </div>
-        </footer>
-      </Link>
-      </div>
-    )
-}
+			<Link href="#hero">
+				<footer className="sticky bottom-5 w-full cursor-pointer">
+					<div className="flex items-center justify-center">
+						<Image
+							src={Profile}
+							alt="profile"
+							className="w-10 h-10 filter grayscale hover:grayscale-0 cursor-pointer rounded-full justify-center"
+						/>
+					</div>
+				</footer>
+			</Link>
+		</div>
+	);
+};
 
 export default Home;
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
-  const pageInfo: PageInfo = await fetchPageInfo();
-  const experiences: Experience[] = await fetchExperiences();
-  const skills: Skill[] = await fetchSkills();
-  const projects: Project[] = await fetchProjects();
-  const socials: Social[] = await fetchSocials();
+	try {
+		const pageInfo: PageInfo = await fetchPageInfo();
+		const experiences: Experience[] = await fetchExperiences();
+		const skills: Skill[] = await fetchSkills();
+		const projects: Project[] = await fetchProjects();
+		const socials: Social[] = await fetchSocials();
 
-  return {
-    props: {
-      pageInfo,
-      experiences,
-      skills,
-      projects,
-      socials,
-    },
-    revalidate: 10,
-  }
-}
+		return {
+			props: {
+				pageInfo,
+				experiences,
+				skills,
+				projects,
+				socials,
+			},
+			revalidate: 10,
+		};
+	} catch (error) {
+		console.log('Error fetching data:', error);
+		throw error;
+	}
+};
