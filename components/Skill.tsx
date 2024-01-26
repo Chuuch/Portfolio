49e9f94e0682fc091bdd46325043637fbf1b284e
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-26 07:25:13 -0800

    fixed skill circles image sized and hover effect size

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -20,12 +20,12 @@ function Skill({ directionLeft, skill }: Props) {
 				src={urlFor(skill?.image).url()}
 				alt="sanity"
 				className="rounded-full border border-gray-500 object-cover
-            w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
+            w-24 h-24 md:w-24 md:h-24 xl:w-28 xl:h-28 filter group-hover:grayscale transition duration-300 ease-in-out"
 			></motion.img>
 
 			<div
 				className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out
-                        group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0"
+                        group-hover:bg-white h-24 w-24 md:w-24 md:h-24 xl:w-28 xl:h-28 rounded-full z-0"
 			>
 				<div className="flex items-center justify-center h-full">
 					<p className="text-3xl font-bold text-black opacity-100">{skill.progress}%</p>
