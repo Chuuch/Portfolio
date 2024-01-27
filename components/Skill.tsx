a17ca2ef5a3ace2b531a997bdfa9bc3b10b89269
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-27 02:10:10 -0800

    responsiveness: skill logo hover size adusted

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -25,7 +25,7 @@ function Skill({ directionLeft, skill }: Props) {
 
 			<div
 				className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out
-                        group-hover:bg-white h-16 w-16 md:w-24 md:h-24 xl:w-28 xl:h-28 rounded-full z-0"
+                        group-hover:bg-white h-14 w-14 md:w-24 md:h-24 lg:w-24 lg:h-24 xl:w-24 xl:h-24 rounded-full z-0"
 			>
 				<div className="flex items-center justify-center h-full">
 					<p className="text-3xl font-bold text-black opacity-100">{skill.progress}%</p>
