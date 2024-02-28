70b9b16649d1faa0136c12c219b346b6ff68268b
Chuuch <daniel.chuchulev96@gmail.com>
2024-02-29 01:21:05 +0200

    removed unused imports

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import Image from 'next/image';
-import sanity from '../public/sanity.png';
 import { Skill } from '@/typings';
 import { urlFor } from '@/sanity';
 
